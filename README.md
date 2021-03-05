dc2654241007b69c022f4d65318b58f7c47d35b1
Carlos Henrique <carlosbda99@hotmail.com>
2021-03-05 10:57:17 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Demonstração de aplicação JAVA com JSF
+
+## Sobre
+
+Este app demonstra uma simples implementação de um gerenciador de tarefas utilizando JSF
+
+Com ele você conseguirá cadastrar suas tarefas e atribuí-las a responsáveis, consultá-las utilizando filtros de Título/Descrição, Responsável e Situação
+
+## Instalação
+
+### No Eclipse
+
+Realize o download do repositório:
+
+> git clone https://github.com/carlosbda99/sample-java-todo
+
+Faça o import na IDE como "Projeto Maven existente"
+
+Execute o projeto e acesse através do endereço:
+
+> http://localhost:<porta_do_server>/sample-java-todo/
+
+## On Going
+
+Integração com banco Postgres utilizando JPA e Hibernates
---
+++ src/main/java/com/appjava/todo/CadastroBean.java
@@ -0,0 +1,174 @@
+package com.appjava.todo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import com.appjava.tarefa.Tarefa;
+
+@ManagedBean
+@ApplicationScoped
+public class CadastroBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private List<Tarefa> tarefas;
+	private List<Tarefa> tarefasPendentes;
+	private List<Tarefa> tarefasFiltradas;
+	private Tarefa tarefaSelecionada;
+	private Tarefa tarefa;
+	private Integer auxiliar;
+	
+	private String tituloDescrFiltro;
+	private String responsavelFiltro;
+	private String situacaoFiltro;
+	private Boolean filtrado;
+	
+	public CadastroBean () {
+		this.tarefa = new Tarefa();
+		this.tarefas = new ArrayList<Tarefa>();
+		this.tarefasPendentes = new ArrayList<Tarefa>();
+		this.tarefasFiltradas = new ArrayList<Tarefa>();
+	}
+	
+	public void resetFiltro() {
+		this.tituloDescrFiltro = "";
+		this.responsavelFiltro = "";
+		this.situacaoFiltro ="";
+		this.filtrado = false;
+	}
+
+	public Boolean getFiltrado() {
+		return filtrado;
+	}
+
+	public String getSituacaoFiltro() {
+		return situacaoFiltro;
+	}
+
+	public void setSituacaoFiltro(String situacaoFiltro) {
+		this.situacaoFiltro = situacaoFiltro;
+	}
+
+	public List<Tarefa> getTarefasPendentes() {
+		return tarefasPendentes;
+	}
+
+	public List<Tarefa> getTarefasFiltradas() {
+		return tarefasFiltradas;
+	}
+
+	public List<Tarefa> getTarefas() {
+		return tarefas;
+	}
+	
+	public void setTarefas(List<Tarefa> tarefas) {
+		this.tarefas = tarefas;
+	}
+	
+	public Tarefa getTarefa() {
+		return tarefa;
+	}
+	
+	public Tarefa getTarefaSelecionada() {
+		return tarefaSelecionada;
+	}
+
+	public void setTarefaSelecionada(Tarefa tarefaSelecionada) {
+		this.tarefaSelecionada = tarefaSelecionada;
+	}
+
+	public Integer getAuxiliar() {
+		return auxiliar;
+	}
+
+	public void setAuxiliar(Integer auxiliar) {
+		this.auxiliar = auxiliar;
+	}
+	
+	public void setResponsavelFiltro(String responsavelFiltro) {
+		this.responsavelFiltro = responsavelFiltro;
+	}
+	
+	public String getResponsavelFiltro() {
+		return responsavelFiltro;
+	}
+	
+	public String getTituloDescrFiltro() {
+		return tituloDescrFiltro;
+	}
+
+	public void setTituloDescrFiltro(String tituloDescrFiltro) {
+		this.tituloDescrFiltro = tituloDescrFiltro;
+	}
+	
+	public Integer getIndex(Tarefa item) {
+		return (this.tarefas.indexOf(item)+1);
+	}
+	
+	public void concluir() {
+		this.auxiliar = this.tarefas.indexOf(this.tarefaSelecionada);
+		this.tarefaSelecionada.setSituacao("Concluída");
+		this.tarefas.set(this.auxiliar, this.tarefaSelecionada);
+		this.tarefasPendentes.remove(this.tarefaSelecionada);
+	}
+	
+	public void excluir() {
+		this.auxiliar = this.tarefas.indexOf(this.tarefaSelecionada);
+		this.tarefaSelecionada.setSituacao("Excluída");
+		this.tarefas.set(this.auxiliar, this.tarefaSelecionada);
+		this.tarefasPendentes.remove(this.tarefaSelecionada);
+		this.tarefasFiltradas.remove(this.tarefaSelecionada);
+	}
+
+	public void incluir() {
+		if ("".equals(this.tarefa.getTitulo())) {
+			this.tarefa.setTitulo("Tarefa " + (this.tarefas.size()+1));
+		}
+		if ("".equals(this.tarefa.getResponsavel())) {
+			this.tarefa.setResponsavel("Não informado");
+		}
+		if ("".equals(this.tarefa.getDescricao())) {
+			this.tarefa.setDescricao("Não informado");
+		}
+		if ("".equals(this.tarefa.getPrioridade())) {
+			this.tarefa.setPrioridade("Alta");
+		}
+		this.tarefa.setId(tarefas.size() + 1);
+		this.tarefa.setSituacao("Pendente");
+		this.tarefas.add(this.tarefa);
+		this.tarefasPendentes.add(this.tarefa);
+		this.tarefa = new Tarefa();
+		this.resetFiltro();
+	}
+	
+//	Implementar filtro de número
+	public void filtrar() {
+		this.tarefasFiltradas.clear();
+		this.filtrado = false;
+		System.out.println(this.responsavelFiltro);
+		System.out.println(this.situacaoFiltro);
+		for (Tarefa item: this.tarefas) {
+			if (( item.getTitulo().startsWith(tituloDescrFiltro) || item.getDescricao().startsWith(tituloDescrFiltro))
+					&& item.getResponsavel().startsWith(this.responsavelFiltro)
+					&& item.getSituacao().startsWith(this.situacaoFiltro))
+				{
+				this.tarefasFiltradas.add(item);
+			}
+			
+		}
+		if (tarefasFiltradas.size() > 0 ) {
+			this.filtrado = true;
+		} else {
+			this.filtrado = false;
+		}
+	}
+	
+	public void limpar() {
+		this.tarefasFiltradas.clear();
+		this.resetFiltro();
+	}
+	
+}
